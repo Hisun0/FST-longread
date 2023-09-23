95f87b2a66d81a6a5353d3682a77e04a1e5ef8fe
Hisun0 <tridvad1@gmail.com>
2023-09-23 14:08:27 +0300

    slider in process

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import Swiper from "swiper/bundle";
+
+export default () => {
+  return new Swiper(".slider-main", {
+    freeMode: true,
+    centeredSlides: true,
+    mousewheel: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 2.5,
+        spaceBetween: 20,
+      },
+      680: {
+        slidesPerView: 3.5,
+        spaceBetween: 60,
+      },
+    },
+  });
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "swiper/css";
+import "./styles/style.css";
+import app from "./app.js";
+
+app();
